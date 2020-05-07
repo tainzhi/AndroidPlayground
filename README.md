a0a388cfc81f822017c4c972b485494c178b4fb0
tainzhi <qfq61@qq.com>
2020-05-07 11:26:59 +0800

    feat: use viewBinding; update readme

--- README.md
+++ README.md
@@ -15,6 +15,20 @@ allprojects {
 
 不能多Application共存, 查询解决办法
 
+### 遇到的问题
+- 多application替换
+- DexArchiveMergerException问题
+- 主题冲突
+- 在子module中未找到资源
+
+### 组件化使用参考
+- [一步步用ARouter实现组件化](https://www.codetd.com/article/11007754)
+- [组件化构想以及ARouter的使用分析](https://www.jianshu.com/p/5b318df8b6f6)
+- [接口+映射解决多application问题](https://www.jianshu.com/p/b9766b1a9c31)
+- [AndroidStudio进行Build时出现DexArchiveMergerException异常的解决办法](https://blog.csdn.net/zgd826237710/article/details/79456202)
+- [组件化过程中application的替换规则](https://blog.csdn.net/baidu_31093133/article/details/94633316)
+- [组件化实现的多媒体应用](https://github.com/guofudong/KotlinAndroid)
+
 ## module/application介绍
 ### module-android-api
 android控件和api使用demo
--- app/src/main/java/com/tanzhi/android/androidsample/MainActivity.kt
+++ app/src/main/java/com/tanzhi/android/androidsample/MainActivity.kt
@@ -4,9 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.GridLayoutManager
 import com.alibaba.android.arouter.launcher.ARouter
-import kotlinx.android.synthetic.main.activity_main.*
+import com.tanzhi.android.androidsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     private val mainAdapter by lazy {
         MainAdapter()
     }
@@ -14,11 +15,12 @@ class MainActivity : AppCompatActivity() {
     private val mainData = ArrayList<MainBean>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         initARouter()
 
-        homeRecyclerView.run {
+        binding.homeRecyclerView.run {
             layoutManager = GridLayoutManager(context, 2);
             adapter = mainAdapter
         }
