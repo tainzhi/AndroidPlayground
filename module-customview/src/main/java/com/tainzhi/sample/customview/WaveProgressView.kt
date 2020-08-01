b6b1c272cfc61e88310c02b984612be1ad9c2496
tainzhi <qfq61@qq.com>
2020-08-01 14:54:09 +0800

    WaveProgressView: add some code

--- module-customview/src/main/java/com/tainzhi/sample/customview/WaveProgressView.kt
+++ module-customview/src/main/java/com/tainzhi/sample/customview/WaveProgressView.kt
@@ -1,21 +1,19 @@
 package com.tainzhi.sample.customview
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.Transformation
 import android.widget.TextView
+import com.tainzhi.sample.customview.Util.dp
 
 /**
  * @author:      tainzhi
  * @mail:        qfq61@qq.com
  * @date:        2020/7/31 22:32
- * @description:
+ * @description: 第二层波浪覆盖第一层波浪上面
  **/
 
 class WaveProgressView @JvmOverloads constructor(
@@ -29,14 +27,91 @@ class WaveProgressView @JvmOverloads constructor(
     // 波浪画笔
     private lateinit var wavePath: Path
     // 第二个波浪画笔
-    private lateinit var secondWavePath: Path
+    private lateinit var secondWavePaint: Paint
 
     private lateinit var bitmap: Bitmap
     private lateinit var bitmapCanvas: Canvas
 
     private lateinit var waveProgressAnim: WaveProgressAnim
     private lateinit var textView: TextView
-    private lateinit var onAni
+    private lateinit var onAnimationListener: OnAnimationListener
+
+    private var waveWidth = 0f
+    private var waveHeight = 0f
+    private var waveNum = 0 // 波浪组的数量, 一次起伏为一组
+    private var waveMovingDistance = 0f // 波浪平移距离
+
+    private var viewSize = 0 // 重新测量后View实际的宽高
+    private var defaultSize = 100.dp() // 自定义View默认的宽高
+
+    private var percent = 0f // 进度条占比
+    private var progressNum = 0f // 可以更新的进度条数值
+    private var maxNum = 100f // 进度条最大值
+
+    private var waveColor = 0 // 波浪颜色
+    private var secondWaveColor = 0 // 第二层波浪颜色
+    private var bgColor = 0 // 背景颜色
+
+    private var isDrawSecondWave = false
+
+    init {
+
+        context.obtainStyledAttributes(
+            attrs,
+            R.styleable.WaveProgressView, defStyleAttr, 0
+        ).apply {
+            waveWidth = getDimension(R.styleable.WaveProgressView_wave_width, 25.dp())
+            waveHeight = getDimension(R.styleable.WaveProgressView_wave_height, 5.dp())
+            waveColor = getColor(R.styleable.WaveProgressView_wave_color, Color.GREEN)
+            secondWaveColor = getColor(R.styleable.WaveProgressView_second_wave_color, Color.BLUE)
+            bgColor = getColor(R.styleable.WaveProgressView_wave_bg_color, Color.GRAY)
+        }.recycle()
+
+        waveNum = (defaultSize / waveWidth / 2).toInt()
+
+        wavePaint = Paint().apply {
+            color = waveColor
+            isAntiAlias = true // 抗锯齿
+            xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
+        }
+        secondWavePaint = Paint().apply {
+            color = secondWaveColor
+            isAntiAlias = true
+            xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_ATOP) // 覆盖在第一层波浪上面, 切要半透明
+        }
+        circlePaint = Paint().apply {
+            color = bgColor
+            isAntiAlias = true
+        }
+        waveProgressAnim = WaveProgressAnim().apply {
+            setAnimationListener(object: Animation.AnimationListener {
+
+            })
+        }
+    }
+
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+        val width = measureSize(defaultSize, widthMeasureSpec)
+        val height = measureSize(defaultSize, heightMeasureSpec)
+        viewSize = Math.min(width, height)
+        setMeasuredDimension(viewSize, viewSize)
+        waveNum = (viewSize / waveWidth / 2).toInt()
+    }
+
+    private fun measureSize(defaultSize: Int, measureSpec: Int): Int {
+        var result = defaultSize
+        val specMode = View.MeasureSpec.getMode(measureSpec)
+        val specSize = MeasureSpec.getSize(measureSpec)
+        if (specMode == MeasureSpec.EXACTLY) {
+            result = specSize
+        } else if (specMode == MeasureSpec.AT_MOST) {
+            result = Math.min(result, specSize)
+        }
+        return result
+    }
 
     interface OnAnimationListener {
 
