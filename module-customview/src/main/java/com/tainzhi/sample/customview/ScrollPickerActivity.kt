897f37e80eaa3364050a656f388c13f0b76f58a4
tainzhi <qfq61@qq.com>
2020-08-18 08:57:23 +0800

    feat: add ScrollerPickerView

--- module-customview/src/main/java/com/tainzhi/sample/customview/ScrollPickerActivity.kt
+++ module-customview/src/main/java/com/tainzhi/sample/customview/ScrollPickerActivity.kt
@@ -1,11 +1,29 @@
 package com.tainzhi.sample.customview
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_scroll_picker.*
+import java.util.concurrent.CopyOnWriteArrayList
 
 class ScrollPickerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scroll_picker)
+
+        val bitmaps = CopyOnWriteArrayList<Bitmap>()
+        bitmaps.add(BitmapFactory.decodeResource(resources, R.drawable.ic_ceo_first_level))
+        bitmaps.add(BitmapFactory.decodeResource(resources, R.drawable.ic_ceo_first_level))
+        bitmaps.add(BitmapFactory.decodeResource(resources, R.drawable.ic_ceo_first_level))
+        bitmaps.add(BitmapFactory.decodeResource(resources, R.drawable.ic_ceo_first_level))
+        bitmaps.add(BitmapFactory.decodeResource(resources, R.drawable.ic_ceo_first_level))
+        bitmaps.add(BitmapFactory.decodeResource(resources, R.drawable.ic_ceo_first_level))
+        bitmaps.add(BitmapFactory.decodeResource(resources, R.drawable.ic_ceo_first_level))
+        bitmapScrollPicker.data = bitmaps
+
+        scrollView.data = arrayListOf<CharSequence>(
+            "one", "two", "three", "four", "five", "six", "seven"
+        )
     }
 }
