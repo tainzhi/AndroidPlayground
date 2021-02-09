f75921b0fc96a8cd47f0c25f0a8e2ef8dc6b4ae1
tainzhi <qfq61@qq.com>
2021-02-09 19:52:11 +0800

    fix github module

--- module-github/src/main/java/com/tainzhi/android/sample/github/di/AppInjector.kt
+++ module-github/src/main/java/com/tainzhi/android/sample/github/di/AppInjector.kt
@@ -44,8 +44,7 @@ object AppInjector {
 
                 }
 
-                override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle?) {
-
+                override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
                 }
 
                 override fun onActivityDestroyed(activity: Activity) {
